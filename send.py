005bd5609267d8da5a5f77bbd25eb592286492ac
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-10-27 16:17:41 +0800

    Code update

--- send.py
+++ send.py
@@ -5,7 +5,7 @@ import sys
 print sys.argv[1]
 
 credentials = pika.PlainCredentials('controller', 'KaraburunCe2')
-parameters = pika.ConnectionParameters('HWLinux',5672,'/',credentials)
+parameters = pika.ConnectionParameters('HWLinux.cloudapp.net',5672,'/',credentials)
 
 connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
