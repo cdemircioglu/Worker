5218b66781d96627538cb1da4ab2c20fee8e700c
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-10-27 15:07:19 +0800

    Code update

---
+++ send.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import pika
+import sys
+
+print sys.argv[1]
+
+credentials = pika.PlainCredentials('controller', 'KaraburunCe2')
+parameters = pika.ConnectionParameters('HWLinux',5672,'/',credentials)
+
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+channel.queue_declare(queue='workresult')
+
+channel.basic_publish(exchange='',routing_key='workresult',body=sys.argv[1])
+
+print(" [x] Sent message!")
+connection.close()
