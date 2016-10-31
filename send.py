5fde48402a2b0680caed8dcab406cae885cd7607
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-10-31 13:09:10 +0800

    Code update

--- send.py
+++ send.py
@@ -5,8 +5,8 @@ import sys
 print sys.argv[1]
 
 credentials = pika.PlainCredentials('controller', 'KaraburunCe2')
-##parameters = pika.ConnectionParameters('HWLinux.cloudapp.net',5672,'/',credentials)
-parameters = pika.ConnectionParameters('localhost',5672,'/',credentials)
+parameters = pika.ConnectionParameters('HWLinux.cloudapp.net',5672,'/',credentials)
+##parameters = pika.ConnectionParameters('localhost',5672,'/',credentials)
 
 connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
@@ -15,5 +15,8 @@ channel.queue_declare(queue='workresult')
 
 channel.basic_publish(exchange='',routing_key='workresult',body=sys.argv[1])
 
+
 print(" [x] Sent message!")
 connection.close()
+
+
