b4b24feafd0c51f96f4d817a5d103d5cbcc63ec6
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-12-22 13:57:46 +0800

    Code update

--- RCode_Worker.R
+++ RCode_Worker.R
@@ -144,7 +144,7 @@ fun_Afflunce <- function()
   #Find the users who would take the offer Response > 0.5
   #src_xdr$ECONOMICBENEFIT[src_xdr$RESPONSE<0.01] <<- -promotionalCost#*src_xdr["RESPONSE"] #The people, not affluent enough, to take the offer.
   src_xdr$ECONOMICBENEFIT[src_xdr$RESPONSE<0.01] <<- -0.0001 #-promotionalCost#*src_xdr["RESPONSE"] #The people, not affluent enough, to take the offer.
-  src_xdr$ECONOMICBENEFIT[src_xdr$RESPONSE<0.03 && src_xdr$RESPONSE > 0.01] <<- -1*promotionalCost*src_xdr["RESPONSE"] #*src_xdr["RESPONSE"] #Customers who use the offer just for a month. 
+  src_xdr$ECONOMICBENEFIT[src_xdr$RESPONSE<0.03 && src_xdr$RESPONSE > 0.01] <<-unlist( -1*promotionalCost*src_xdr["RESPONSE"]) #*src_xdr["RESPONSE"] #Customers who use the offer just for a month. 
   
   #src_xdr$ECONOMICBENEFIT[src_xdr$ECONOMICBENEFIT< -200] <<- -200 #Lowend
   #src_xdr$ECONOMICBENEFIT[src_xdr$ECONOMICBENEFIT> 1000] <<- 1000 #Highend
